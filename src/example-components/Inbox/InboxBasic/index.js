86192cfbcef6191cb5b84c030b801dbf8f36b578
Emmanuel Nwachi <egreatness1@gmail.com>
2021-02-07 01:34:10 +0100

    fix bugs

--- src/example-components/Inbox/InboxBasic/index.js
+++ src/example-components/Inbox/InboxBasic/index.js
@@ -1,17 +1,29 @@
 import React, { Fragment, useState } from 'react';
-import { Grid, GridItem, Icon, Link, Text, VStack } from '@chakra-ui/react';
+import {
+  Center,
+  Grid,
+  GridItem,
+  Icon,
+  Link,
+  Text,
+  VStack
+} from '@chakra-ui/react';
 import { HiInboxIn } from 'react-icons/hi';
 import { FaMobileAlt } from 'react-icons/fa';
+import { Divider } from '@material-ui/core';
 
 export default function LivePreviewExample() {
   const [messages] = useState('No messages yet');
   return (
     <Fragment>
-      <Grid templateColumns="repeat(5, 1fr)" gap={4} maxW="600px" mx="auto">
-        <VStack colSpan={4}>
+      <Grid templateColumns="repeat(5, 1fr)" gap={4} maxW="700px" mx="auto">
+        <VStack>
           <Icon as={HiInboxIn} w="200px" h="170px" color="#95c53e" />
           <Text>{messages}</Text>
         </VStack>
+        <Center height="100vh">
+          <Divider orientation="vertical" />
+        </Center>
         <GridItem colStart={4} colEnd={6} gridRowGap={4}>
           <Icon
             as={FaMobileAlt}
