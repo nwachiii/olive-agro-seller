a6872e57ca8e5411217a106bf606d551b3145bfb
Emmanuel Nwachi <egreatness1@gmail.com>
2021-02-08 10:22:06 +0100

    add hover states on tablist

--- src/layout-components/OliveOrders/AllOrders/index.js
+++ src/layout-components/OliveOrders/AllOrders/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { Tag, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import ToBeConfirmed from './ToBeConfirmed';
 import Unpaid from './Unpaid';
 import ToBeShipped from './ToBeShipped';
@@ -11,16 +11,117 @@ import AllOrders from './AllOrders';
 function AllOrdersIndex() {
   return (
     <Tabs isLazy borderRadius="10px" border="1px solid lightgray">
-      <TabList px="2em" py="1em" my="auto" borderBottom="1px solid lightgray">
-        <Tab border="none" borderRight="0.5px solid darkgray">
+      <TabList
+        variant="solid"
+        px="2em"
+        pt="1em"
+        my="auto"
+        borderBottom="1px solid lightgray">
+        <Tab
+          defaultIndex={1}
+          border="none"
+          borderRight="1.5px solid darkgray"
+          mr={2}
+          _hover={{
+            background: '#95c53e'
+          }}
+          _active={{
+            borderBottom: '3px solid #95c53e'
+          }}
+          _focus={{
+            borderBottom: '3px solid #95c53e'
+          }}>
           All
         </Tab>
-        <Tab border="none">To be Confirmed</Tab>
-        <Tab border="none">Unpaid</Tab>
-        <Tab border="none">To be Shipped</Tab>
-        <Tab border="none">To be received</Tab>
-        <Tab border="none">In Dispute</Tab>
-        <Tab border="none">Completed/Closed</Tab>
+        <Tab
+          border="none"
+          borderRight="1.5px solid darkgray"
+          mr={2}
+          _hover={{
+            background: '#95c53e'
+          }}
+          _active={{
+            borderBottom: '3px solid #95c53e'
+          }}
+          _focus={{
+            borderBottom: '3px solid #95c53e'
+          }}>
+          To be Confirmed
+        </Tab>
+        <Tab
+          border="none"
+          borderRight="1.5px solid darkgray"
+          mr={2}
+          _hover={{
+            background: '#95c53e'
+          }}
+          _active={{
+            borderBottom: '3px solid #95c53e'
+          }}
+          _focus={{
+            borderBottom: '3px solid #95c53e'
+          }}>
+          Unpaid
+        </Tab>
+        <Tab
+          border="none"
+          borderRight="1.5px solid darkgray"
+          mr={2}
+          _hover={{
+            background: '#95c53e'
+          }}
+          _active={{
+            borderBottom: '3px solid #95c53e'
+          }}
+          _focus={{
+            borderBottom: '3px solid #95c53e'
+          }}>
+          To be Shipped
+        </Tab>
+        <Tab
+          border="none"
+          borderRight="1.5px solid darkgray"
+          mr={2}
+          _hover={{
+            background: '#95c53e'
+          }}
+          _active={{
+            borderBottom: '3px solid #95c53e'
+          }}
+          _focus={{
+            borderBottom: '3px solid #95c53e'
+          }}>
+          To be received
+        </Tab>
+        <Tab
+          border="none"
+          borderRight="1.5px solid darkgray"
+          mr={2}
+          _hover={{
+            background: '#95c53e'
+          }}
+          _active={{
+            borderBottom: '3px solid #95c53e'
+          }}
+          _focus={{
+            borderBottom: '3px solid #95c53e'
+          }}>
+          In Dispute
+        </Tab>
+        <Tab
+          border="none"
+          mr={2}
+          _hover={{
+            background: '#95c53e'
+          }}
+          _active={{
+            borderBottom: '3px solid #95c53e'
+          }}
+          _focus={{
+            borderBottom: '3px solid #95c53e'
+          }}>
+          Completed/Closed
+        </Tab>
       </TabList>
       <TabPanels p="30px" display="flex" justifyContent="center" w="100%">
         <TabPanel>
