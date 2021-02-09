e213038e2ad6c2a17ad96db137ca5d85c778fa50
Emmanuel Nwachi <egreatness1@gmail.com>
2021-02-09 21:58:11 +0100

    update home pagination

--- src/example-components/Pagination/PaginationBasic/index.js
+++ src/example-components/Pagination/PaginationBasic/index.js
@@ -100,19 +100,19 @@ function createData(name, calories, fat) {
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7),
-  createData('Donut', 452, 25.0),
-  createData('Eclair', 262, 16.0),
-  createData('Frozen yoghurt', 159, 6.0),
-  createData('Gingerbread', 356, 16.0),
-  createData('Honeycomb', 408, 3.2),
-  createData('Ice cream sandwich', 237, 9.0),
-  createData('Jelly Bean', 375, 0.0),
-  createData('KitKat', 518, 26.0),
-  createData('Lollipop', 392, 0.2),
-  createData('Marshmallow', 318, 0),
-  createData('Nougat', 360, 19.0),
-  createData('Oreo', 437, 18.0)
+  createData('Drinks', 305, 3.7),
+  createData('Dry Herbs', 452, 25.0),
+  createData('Flours', 262, 16.0),
+  createData('Fruits and Vegetables', 159, 6.0),
+  createData('Legumes', 356, 16.0),
+  createData('Oils', 408, 3.2),
+  createData('ewedu', 237, 9.0),
+  createData('Spices', 375, 0.0),
+  createData('moi moi leaf', 518, 26.0),
+  createData('uziza', 392, 0.2),
+  createData('bitter-leaf', 318, 0),
+  createData('pawpaw roots and leaves', 360, 19.0),
+  createData('Beans', 437, 18.0)
 ].sort((a, b) => (a.calories < b.calories ? -1 : 1));
 
 const useStyles2 = makeStyles({
