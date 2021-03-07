96769bcb71a137ad63a252f7fd4ede1f4b51315c
Emmanuel Nwachi <egreatness1@gmail.com>
2021-03-07 18:00:39 +0100

    fix auth bug, display logged in user

--- src/layout-components/Cards/HomeFirstCard/index.js
+++ src/layout-components/Cards/HomeFirstCard/index.js
@@ -17,10 +17,10 @@ export default function HomeFirstCard() {
               </a>
               <div className="flex-grow-1 mt-4">
                 <h1 className="text-black-40">
-                  {` ${user.firstName} ${user.lastName}`}
+                  {user ? ` ${user.firstName} ${user.lastName}` : ""}
                 </h1>
                 <a className="text-black-50 pt-2" href="/#">
-                  <h5>View Profile</h5>
+                  {user ? <h5>View Profile</h5> : ""}
                 </a>
               </div>
             </div>
