ccabf71917754be362537f746bde8e4f27963f42
Emmanuel Nwachi <egreatness1@gmail.com>
2021-03-08 09:55:06 +0100

    change register auth url to admin's

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-// import rootReducer from "./redux/reducers/rootReducer";
 
-// const store = createStore(rootReducer, applyMiddleware(thunk));
-
-ReactDOM.render(
-  // <Provider store={store}>
-  <App />,
-  // </Provider>
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
